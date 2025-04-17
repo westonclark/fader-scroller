528bff02ce448d041a2242a20c8805181a3fe1c2
Weston Clark <westoclark@gmail.com>
2025-04-17 15:23:58 -0700

    Initial Commit

---
+++ fader-scroller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fader-scroller
+//
+//  Created by Weston Clark on 4/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fader-scroller/fader_scrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fader_scrollerApp.swift
+//  fader-scroller
+//
+//  Created by Weston Clark on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct fader_scrollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
